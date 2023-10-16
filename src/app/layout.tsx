7bbafeffdb3efe52096e71ea6a7e54b80fd184fd
Iqbal Sahrizal <iqbal.sahrizal@dompetkilat.co.id>
2023-10-16 21:11:42 +0700

    setup database

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
+import Providers from "@/components/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +15,12 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={cn("min-h-screen font-sans antialiased grainy", inter.className)}>
-        <Navbar />
-        {children}
-      </body>
+      <Providers>
+        <body className={cn("min-h-screen font-sans antialiased grainy", inter.className)}>
+          <Navbar />
+          {children}
+        </body>
+      </Providers>
     </html>
   );
 }
