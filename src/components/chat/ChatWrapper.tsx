28af43866a2051cdb573776eac056e2c5caaea93
Iqbal Sahrizal <iqbal.sahrizal@dompetkilat.co.id>
2023-10-18 13:29:24 +0700

    sub

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -2,17 +2,19 @@
 
 import { trpc } from "@/app/_trpc/client";
 import ChatInput from "./ChatInput";
+import Messages from "./Messages";
 import { ChevronLeft, Loader2, XCircle } from "lucide-react";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
 import { ChatContextProvider } from "./ChatContext";
-import Messages from "./Messages";
+import { PLANS } from "@/config/stripe";
 
 interface ChatWrapperProps {
   fileId: string;
+  isSubscribed: boolean;
 }
 
-const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
+const ChatWrapper = ({ fileId, isSubscribed }: ChatWrapperProps) => {
   const { data, isLoading } = trpc.getFileUploadStatus.useQuery(
     {
       fileId,
@@ -60,7 +62,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
             <XCircle className="h-8 w-8 text-red-500" />
             <h3 className="font-semibold text-xl">Too many pages in PDF</h3>
             <p className="text-zinc-500 text-sm">
-              Your <span className="font-medium">Free</span> plan supports up to 5 pages per PDF.
+              Your <span className="font-medium">{isSubscribed ? "Pro" : "Free"}</span> plan supports up to {isSubscribed ? PLANS.find((p) => p.name === "Pro")?.pagesPerPdf : PLANS.find((p) => p.name === "Free")?.pagesPerPdf} pages per PDF.
             </p>
             <Link
               href="/dashboard"
@@ -85,6 +87,7 @@ const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
         <div className="flex-1 justify-between flex flex-col mb-28">
           <Messages fileId={fileId} />
         </div>
+
         <ChatInput />
       </div>
     </ChatContextProvider>
